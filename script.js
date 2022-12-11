744bc3265cd5a310be32ac95f7d7adfe47fc0874
arslonga83 <jlwood83@gmail.com>
2022-12-11 15:12:54 -0800

    add render character function

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-document.querySelector('#hero').innerHTML = `
-   <div class="character-card">
-      <h4 class="name"> Wizard </h4>
-      <img class="avatar" src="images/wizard.png"/>
-      <p class="health">health: <b> 60 </b></p>
-      <div class="dice-container"><div class="dice"> 6 </div></div>
-   </div>   
+const hero = {
+   id: 'hero',
+   name: 'Wizard',
+   avatar: 'images/wizard.png',
+   health: '60',
+   dice: '6'
+}
+
+const monster = {
+   id: 'monster', 
+   name: 'Orc',
+   avatar: 'images/orc.png',
+   health: '10', 
+   dice: '4'
+}
+
+function renderCharacter(data) {
+   document.getElementById(data.id).innerHTML += `
+      <div class="character-card">
+         <h4 class="name"> ${data.name} </h4>
+         <img class="avatar" src="${data.avatar}"/>
+         <p class="health">health: <b> ${data.health} </b></p>
+         <div class="dice-container"><div class="dice"> ${data.dice} </div></div>
+      </div>   
 `
-document.querySelector('#monster').innerHTML = `
-   <div class="character-card">
-      <h4 class="name"> Orc </h4>
-      <img class="avatar" src="images/orc.png"/>
-      <p class="health">health: <b> 10 </b></p>
-      <div class="dice-container"><div class="dice"> 4 </div></div>
-   </div>
-`
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
