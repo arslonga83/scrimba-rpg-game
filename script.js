8fc607dd5d498ad8374b4bc1db6ec56e4d6bc47d
arslonga83 <jlwood83@gmail.com>
2022-12-11 22:10:03 -0800

    render dice arrays with for loop

--- script.js
+++ script.js
@@ -2,26 +2,34 @@ const hero = {
    id: 'hero',
    name: 'Wizard',
    avatar: 'images/wizard.png',
-   health: '60',
-   dice: '6'
+   health: 60,
+   dice: [3, 1, 4],
+   diceCount: 3
 }
 
 const monster = {
    id: 'monster', 
    name: 'Orc',
    avatar: 'images/orc.png',
-   health: '10', 
-   dice: '4'
+   health: 10, 
+   dice: [2],
+   diceCount: 1
 }
 
 function renderCharacter(data) {
-   const { id, name, avatar, health, dice } = data;
+   const { id, name, avatar, health, dice, diceCount } = data;
+   let diceHtml = '';
+
+   for (let i = 0; i < diceCount; i++) {
+      diceHtml += `<div class="dice">${dice[i]}</div>`
+   }
+
    document.getElementById(id).innerHTML += `
       <div class="character-card">
          <h4 class="name"> ${name} </h4>
          <img class="avatar" src="${avatar}"/>
          <p class="health">health: <b> ${health} </b></p>
-         <div class="dice-container"><div class="dice"> ${dice} </div></div>
+         <div class="dice-container">${diceHtml}</div>
       </div>   
 `
 }
